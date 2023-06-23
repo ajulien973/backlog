6ba95d9d126fd903925a5f7ffae5d25bca352a66
Alexis Julien <aju@Alexiss-MacBook-Pro.local>
2023-06-23 13:22:26 +0900

    build movie list page within main App

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,32 @@
+import Title from "components/atoms/typography/title";
+import Card from "components/molecules/card";
+import movies from "app/movies-list";
 import styles from "./app.module.css";
 
 const App = (): JSX.Element => {
   return (
     <main className={styles.main}>
       <header className={styles.header}>
-        <h3 className={styles.headerTopTitle}>
-          <span className={styles.headerTopTitleVital}>Vital</span> @ Vite
-          Template
-        </h3>
+        <Title>Popular movies</Title>
       </header>
+      <section className={styles.movies}>
+        {movies.results.map((props, index) => (
+          <div
+            key={index}
+            className={styles.cardWrapper}
+            style={{ animationDelay: `${index * 0.1 + 0.1}s` }}
+          >
+            <Card
+              title={props.title}
+              releaseDate={props.released_on}
+              subjectId={props.id}
+            />
+          </div>
+        ))}
+      </section>
       <footer className={styles.footer}>
-        <a href="https://github.com/jvidalv">
-          Josep Vidal @ {new Date().getFullYear()}
+        <a href="https://github.com/ajulien973">
+          Alexis Julien @ {new Date().getFullYear()}
         </a>
       </footer>
     </main>
