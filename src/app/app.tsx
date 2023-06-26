0547ada59030daf64c538fc75657308aa7ce4c1a
Alexis Julien <aju@Alexiss-MacBook-Pro.local>
2023-06-26 14:48:50 +0900

    Use new Header, Title, MediaList and Footer in root App

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,34 +1,20 @@
+import Header from "components/organisms/header";
 import Title from "components/atoms/typography/title";
-import Card from "components/molecules/card";
+import MediaList from "components/organisms/media-list";
+import Footer from "components/organisms/footer";
 import movies from "app/movies-list";
 import styles from "./app.module.css";
 
 const App = (): JSX.Element => {
   return (
     <main className={styles.main}>
-      <header className={styles.header}>
+      <Header>
         <Title>Popular movies</Title>
-      </header>
-      <section className={styles.movies}>
-        {movies.results.map((props, index) => (
-          <div
-            key={index}
-            className={styles.cardWrapper}
-            style={{ animationDelay: `${index * 0.1 + 0.1}s` }}
-          >
-            <Card
-              title={props.title}
-              releaseDate={props.released_on}
-              subjectId={props.id}
-            />
-          </div>
-        ))}
+      </Header>
+      <section>
+        <MediaList mediaList={movies.results} />
       </section>
-      <footer className={styles.footer}>
-        <a href="https://github.com/ajulien973">
-          Alexis Julien @ {new Date().getFullYear()}
-        </a>
-      </footer>
+      <Footer />
     </main>
   );
 };
