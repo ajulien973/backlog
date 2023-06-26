5ff4d9825a7f97beda1e7ccc01908b86dc7a1e89
Alexis Julien <aju@Alexiss-MacBook-Pro.local>
2023-06-26 15:26:24 +0900

    Use Page template in App

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,20 +1,16 @@
-import Header from "components/organisms/header";
-import Title from "components/atoms/typography/title";
+import Page from "components/templates/page";
 import MediaList from "components/organisms/media-list";
-import Footer from "components/organisms/footer";
 import movies from "app/movies-list";
 import styles from "./app.module.css";
 
 const App = (): JSX.Element => {
   return (
     <main className={styles.main}>
-      <Header>
-        <Title>Popular movies</Title>
-      </Header>
-      <section>
-        <MediaList mediaList={movies.results} />
-      </section>
-      <Footer />
+      <Page title={"Popular movies"}>
+        <section>
+          <MediaList mediaList={movies.results} />
+        </section>
+      </Page>
     </main>
   );
 };
