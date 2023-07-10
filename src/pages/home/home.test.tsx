0a9f6245fa1c05de81997f0ee07b6c958f91ffab
Alexis Julien <aju@Alexiss-MacBook-Pro.local>
2023-07-10 13:03:42 +0900

    Setup miragejs with dummy data

--- src/pages/home/home.test.tsx
+++ src/pages/home/home.test.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Server } from "miragejs";
 
 import { Home } from "./home";
+import { makeServer } from "mirage/server";
 import { render } from "@testing-library/react";
 
 describe("Home Page", () => {
-  test("should match snapshot", () => {
-    const page = render(<Home />);
+  let server: Server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: process.env.NODE_ENV });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  test("should match snapshot", async () => {
+    const page = await render(<Home />);
 
     expect(page).toMatchSnapshot();
   });
