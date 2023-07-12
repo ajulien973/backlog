60b635bb3fa80ebb9943365c2046aea26381431d
Alexis Julien <aju@Alexiss-MacBook-Pro.local>
2023-07-12 16:15:28 +0900

    Add content type support to Card component

--- src/components/molecules/card/card.test.tsx
+++ src/components/molecules/card/card.test.tsx
@@ -7,16 +7,64 @@ describe("Card test", () => {
     const title = "Star Wars";
     const releasedOn = "2022-12-14T00:00:00";
     const id = "c77578d5-2736-4da6-9e8e-269509c5ef61";
+    const contentType = "m";
 
     const { getByText, getByRole } = render(
-      <Card title={title} releaseDate={releasedOn} subjectId={id} />
+      <Card
+        title={title}
+        releaseDate={releasedOn}
+        subjectId={id}
+        type={contentType}
+      />
     );
 
-    expect(getByRole("img")).toHaveAttribute(
-      "src",
-      "https://img.rgstatic.com/content/movie/c77578d5-2736-4da6-9e8e-269509c5ef61/poster-342.jpg"
-    );
     expect(getByText("Star Wars")).toBeInTheDocument();
     expect(getByText("Wed Dec 14 2022")).toBeInTheDocument();
+  });
+
+  describe("When content is a movie", () => {
+    test("Should render the poster with the right url", () => {
+      const title = "Star Wars";
+      const releasedOn = "2022-12-14T00:00:00";
+      const id = "c77578d5-2736-4da6-9e8e-269509c5ef61";
+      const contentType = "m";
+
+      const { getByRole } = render(
+        <Card
+          title={title}
+          releaseDate={releasedOn}
+          subjectId={id}
+          type={contentType}
+        />
+      );
+
+      expect(getByRole("img")).toHaveAttribute(
+        "src",
+        "https://img.rgstatic.com/content/movie/c77578d5-2736-4da6-9e8e-269509c5ef61/poster-342.jpg"
+      );
+    });
+  });
+
+  describe("When content is a tv show", () => {
+    test("Should render the poster with the right url", () => {
+      const title = "Star Wars";
+      const releasedOn = "2022-12-14T00:00:00";
+      const id = "c77578d5-2736-4da6-9e8e-269509c5ef61";
+      const contentType = "s";
+
+      const { getByRole } = render(
+        <Card
+          title={title}
+          releaseDate={releasedOn}
+          subjectId={id}
+          type={contentType}
+        />
+      );
+
+      expect(getByRole("img")).toHaveAttribute(
+        "src",
+        "https://img.rgstatic.com/content/show/c77578d5-2736-4da6-9e8e-269509c5ef61/poster-342.jpg"
+      );
+    });
   });
 });
