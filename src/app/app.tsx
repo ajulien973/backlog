d3d7c3a4f32664c509c8506f7bcf3a6c611b6a15
Alexis Julien <aju@Alexiss-MacBook-Pro.local>
2023-07-12 15:21:31 +0900

    use router for moving between tv and movie pages

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,25 @@
 import Home from "pages/home";
+import Series from "pages/series";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import styles from "./app.module.css";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route index element={<Home />} />
+      <Route path="tv" element={<Series />} />
+    </>
+  )
+);
 const App = (): JSX.Element => {
   return (
     <main className={styles.main}>
-      <Home />
+      <RouterProvider router={router} />
     </main>
   );
 };
